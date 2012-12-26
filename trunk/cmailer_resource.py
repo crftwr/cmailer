d814563c0518743021f7e28dc56ee25a4b4a5f05
craftware <craftware@2e6ec548-719f-dcb8-119a-4dd6a084a310>
2012-12-26 13:42:18 +0000

    CraftMailerの準備

---
+++ trunk/cmailer_resource.py
@@ -0,0 +1,12 @@
+﻿
+cfiler_appname = u"内骨格"
+cfiler_dirname = u"CraftFiler"
+cfiler_version = "2.20"
+
+_startup_string_fmt = u"""\
+%s version %s:
+  http://sites.google.com/site/craftware/
+"""
+
+def startupString():
+    return _startup_string_fmt % ( cfiler_appname, cfiler_version )
